a95098e48a2f790fad713629e37a4f39611389d3
souken-b <yuto430scuderia@gmail.com>
2020-04-14 23:11:10 +0900

    インデントを修正

--- resources/views/ajax-crud.blade.php
+++ resources/views/ajax-crud.blade.php
@@ -7,11 +7,10 @@
   <meta name="csrf-token" content="{{ csrf_token() }}">
   <title>laravel 6 First Ajax CRUD Application - W3path.com</title>
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.3/css/bootstrap.min.css" />
-<script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.19.0/jquery.validate.js"></script>
+  <script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.19.0/jquery.validate.js"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
   <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
- 
  <style>
    .container{
     padding: 0.5%;
